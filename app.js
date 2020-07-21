d98a1bbdb4ddff08a7ac16695293c80f4fc8d2aa
McFlacken <angel.i.martinez@gmail.com>
2020-07-20 19:59:16 -0600

    put randomCat inside the GET route so it's run everytime the route is called

--- app.js
+++ app.js
@@ -9,25 +9,16 @@ app.set("view engine", "ejs");
 //this is a hard-coded variable that stores a few cat image urls
 var cats = ["https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1569591159212-b02ea8a9f239?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1547045662-e5a75e7238c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1552933529-e359b2477252?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"];
 
-var randomCat = (function() {
-    var randCat;
-	randCat = cats[Math.floor(Math.random()*cats.length)];
-    return randCat;
-})(); 
-
-//this is the function that selects a random url from the array defined above and displays it
-// function getCat(){
-// 	randomCat = cats[Math.floor(Math.random()*cats.length)];	
-// };
-
-// //this calls the function so the first image is displayed when the page loads
-// getCat();
-
 app.get("/", function(req,res){
 	res.redirect("cats");
 });
 
 app.get("/cats", function(req,res){
+	var randomCat = (function() {
+		var randCat;
+		randCat = cats[Math.floor(Math.random()*cats.length)];
+		return randCat;
+	})();
 	res.render("cats", {randomCat: randomCat});
 });
 
